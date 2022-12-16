395c60266dcbf4fdbe270023b9082757d02e415b
lucierou <lucie.rouault@live.fr>
2022-12-16 10:24:20 +0100

    exercice terminé

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,78 @@
 package org.example;
 
+import org.example.model.Produit;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercice1");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+
+        Produit produit;
+        for (int i=0; i<5; i++) {
+            produit = new Produit();
+            produit.setMarque("marque" + (i+1));
+            produit.setReference("reference" + (i+1));
+            produit.setDateAchat(new Date());
+            produit.setPrix(25.0);
+            produit.setStock(i);
+
+            em.persist(produit);
+            transaction.commit();
+            transaction.begin();
+            produit = null;
+        }
+
+        System.out.println("1 : Liste des produits : ");
+        Query query1 = em.createQuery("select p from Produit p");
+        List produits = query1.getResultList();
+        for (Object prod : produits) {
+            Produit tmp = (Produit) prod;
+            System.out.println(tmp);
+        }
 
 
+        System.out.println("2 : Affichage produit avec id = 2 : ");
+        Produit p2 = em.find(Produit.class, 2);
+        System.out.println(p2);
+        transaction.commit();
+
+
+
+        System.out.println("3: Suppression du produit avec id = 3 :");
+        transaction.begin();
+        Produit p3 = em.find(Produit.class, 3);
+        em.remove(p3);
+        transaction.commit();
+        Query query3 = em.createQuery("select p from Produit p");
+        List prods = query3.getResultList();
+        for (Object prod : prods) {
+            Produit tmp = (Produit) prod;
+            System.out.println(tmp);
+        }
+
+
+
+        System.out.println("4: Modification du produit avec id = 1 :");
+        transaction.begin();
+
+        Produit p1 = em.find(Produit.class, 1);
+        System.out.println("Produit avec id = 1 (avant modif) : " + p1);
+        p1.setMarque("marque modifiée");
+        p1.setReference("référence modifiée");
+        em.flush();
+        transaction.commit();
+
+        Produit p4 = em.find(Produit.class, 1);
+        System.out.println("Après modification : " + p4);
 
     }
 }
---
+++ src/main/java/org/example/model/Produit.java
@@ -0,0 +1,82 @@
+package org.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Produit {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String marque;
+    private String reference;
+    private Date dateAchat;
+    private Double prix;
+    private int stock;
+
+    public Produit() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id=" + id +
+                ", marque='" + marque + '\'' +
+                ", reference='" + reference + '\'' +
+                ", dateAchat=" + dateAchat +
+                ", prix=" + prix +
+                ", stock=" + stock +
+                '}';
+    }
+}
